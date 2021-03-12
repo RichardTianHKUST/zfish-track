261688d69bda6b3c49b35260bbd3b7d5d649cdbb
kclamar <kclamar@connect.ust.hk>
2021-03-12 18:28:58 +0800

    bug fix

--- free_swim_eye_tracker/select_params_batch.py
+++ free_swim_eye_tracker/select_params_batch.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from free_swim_eye_tracker.utils.config import config_suffix
 from free_swim_eye_tracker.utils.io import ask_directory, ask_filenames, get_file
 from free_swim_eye_tracker.select_params import ParameterSelector
 
@@ -10,7 +11,7 @@ def batch_select_parameters_from_videos(video_paths=None, method='binary', roi=T
     prev_params = {}
     for video_path in video_paths:
         if skip_processed_videos:
-            if get_file(video_path, ParameterSelector.config_suffix).exists():
+            if get_file(video_path, config_suffix).exists():
                 continue
         selector = ParameterSelector(video_path, roi, method=method, params=prev_params)
         if start_from_prev_params:
