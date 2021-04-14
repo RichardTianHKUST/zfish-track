f1b67052502b55d4d74a0173a9be59478214ec7e
kclamar <kclamar@connect.ust.hk>
2021-04-15 02:56:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
-setuptools.setup(
-    name="zfish-track",
-    description="",
+setup(
+    name='zfish-track',
+    version='1.0',
+    description='Pose estimation for larval zebrafish',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="",
-    packages=setuptools.find_packages(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/SemmelhackLab/zfish-track',
+    packages=find_packages(),
     install_requires=[
-        "imageio-ffmpeg",
-        "numpy",
-        "opencv-python>4.5.1",
-        "pandas",
-        "pywin32",
-        "scikit-image",
-        "tkfilebrowser",
-        "tqdm",
+        'imageio-ffmpeg',
+        'numpy',
+        'opencv-python>4.5.1',
+        'pandas',
+        'pywin32',
+        'scikit-image',
+        'tkfilebrowser',
+        'tqdm',
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
     ],
     python_requires='>=3.7',
 )
